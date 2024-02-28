772452cf5829b1c639ca016c4061acace93f953d
viisualworks <dshege4@gmail.com>
2024-02-28 16:41:38 +0300

    add button styles to homepage buttons

--- national-assembly/themes/custom/visualstheme/js/homepage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let splide_hero = new Splide('.splide.hero-images', {
-    type: 'loop',
-    perPage: 1,
-    autoplay: true,
-    speed: 300,
-  });
-  splide_hero.mount();
-});
