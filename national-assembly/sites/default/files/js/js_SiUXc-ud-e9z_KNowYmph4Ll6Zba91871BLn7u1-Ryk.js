75277054757ad2e8e28f9a12a4fe4c1441dfe950
viisualworks <dshege4@gmail.com>
2024-05-14 18:51:37 +0300

    edit the header and footer for consistency

---
+++ national-assembly/sites/default/files/js/js_SiUXc-ud-e9z_KNowYmph4Ll6Zba91871BLn7u1-Ryk.js
@@ -0,0 +1,3 @@
+/* @license GNU-GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+;
+;
--- national-assembly/sites/default/files/js/js_e7ppYgIjXa1Hlrn_OaVKc_AJQ5bR4kyiv226ACQzPXQ.js
+++
@@ -1,3 +0,0 @@
-/* @license GNU-GPL-2.0-or-later https://www.drupal.org/licensing/faq */
-;
-document.addEventListener("DOMContentLoaded",function(){let splide_hero=new Splide('.splide.hero-images',{type:'loop',perPage:1,autoplay:true,speed:300});splide_hero.mount();});;
