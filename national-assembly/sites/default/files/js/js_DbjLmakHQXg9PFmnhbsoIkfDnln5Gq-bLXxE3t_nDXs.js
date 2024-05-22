b14f6a6f7e5a4913417463c78e81e3bb2ea4a883
viisualworks <dshege4@gmail.com>
2024-05-22 18:42:08 +0300

    edit the national assembly schedule block

--- national-assembly/sites/default/files/js/js_DbjLmakHQXg9PFmnhbsoIkfDnln5Gq-bLXxE3t_nDXs.js
+++
@@ -1,3 +0,0 @@
-/* @license GNU-GPL-2.0-or-later https://www.drupal.org/licensing/faq */
-;
-document.addEventListener("DOMContentLoaded",function(){let more_articles=new Splide('.more-articles .splide',{arrows:false,perPage:3,pagination:true,autoplay:true,speed:250,breakpoints:{1024:{perPage:2},640:{perPage:1}}});more_articles.mount();});;
